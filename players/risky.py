898524b9a8ba78949b12e9596173df8a83327a39
Benedetto <s247082@studenti.polito.it>
2023-03-16 19:00:53 +0100

    feat: implementation of risky strategy

--- players/risky.py
+++ players/risky.py
@@ -2,18 +2,168 @@
 
 from quarto import quarto
 import random
+import numpy as np
+from functools import reduce
+from copy import deepcopy
 
 class RiskyPlayer(quarto.Player):
 
-
     def __init__(self, quarto: quarto.Quarto) -> None:
         super().__init__(quarto)
+        self.available_position = [(i, j) for i in range(4) for j in range(4)]
+        self.available_pieces = [i for i in range(16)]
 
+    def __get_board(self):
+        game = self.get_game()
+        board = game.get_board_status()
+        return board
+
+    def __simulate_winning_move(self, position, piece=None):
+        quarto = deepcopy(self.get_game())
+        if piece:
+            quarto.select(piece)
+        quarto.place(position[0], position[1])
+        return quarto.check_winner != -1
+
+    def __update_pieces(self):
+        board = self.__get_board()
+        pieces_index = np.where(board != -1)
+        placed_pieces_on_board = board[pieces_index]
+        for i in placed_pieces_on_board:
+            if i in self.available_pieces:
+                self.available_pieces.remove(i)
+
+    def __update_positions(self):
+        board = self.__get_board()
+        pieces_index = np.where(board != -1)
+        for i in range(len(pieces_index[0])):
+            tuple_index = (pieces_index[1][i], pieces_index[0][i])
+            if tuple_index in self.available_position:
+                self.available_position.remove(tuple_index)
+
+    def __check_winning_position(self, positions, partial_board):
+        if positions.shape[0] == 1:
+            binary_representation = partial_board
+            binary_representation = binary_representation[binary_representation != -1]
+            common_pieces_1 = reduce(lambda a, b: a & b, binary_representation)
+            common_pieces_2 = reduce(lambda a, b: a & (not b), binary_representation, 15)
+            return common_pieces_1 or common_pieces_2
+        return False
+
+    def __list_winning_positions(self):
+        # 1 check if it's the last position where to place a piece
+        winning_position = []
+
+        if len(self.available_pieces) > 12:
+            return winning_position
+
+        board = self.__get_board()
+
+        for i in range(4):
+            column_pos = np.where(board[i, :] == -1)[0]
+            if self.__check_winning_position(column_pos, board[i, :]):
+                winning_position.append((column_pos[0], i))
+
+            row_pos = np.where(board[:, i] == -1)[0]
+            if self.__check_winning_position(row_pos, board[:, i]):
+                winning_position.append((i, row_pos[0]))
+
+        diag_1 = np.diag(board)
+        diag1_pos = np.where(diag_1 == -1)[0]
+        if self.__check_winning_position(diag1_pos, diag_1):
+            winning_position.append((diag1_pos[0], diag1_pos[0]))
+
+        diag_2 = np.diag(np.fliplr(board))
+        diag2_pos = np.where(diag_2 == -1)[0]
+        if self.__check_winning_position(diag2_pos, diag_2):
+            winning_position.append((3 - diag2_pos[0], diag2_pos[0]))
+
+        return winning_position
 
     def choose_piece(self) -> int:
-        #print(self._Player__quarto.get_board_status())
-        return random.randint(0, 15)
+        self.__update_pieces()
+        # check if there is a winning position for the opponent
+        winning_position_opponent = self.__list_winning_positions()
+        if len(winning_position_opponent) > 0:
+            # if there is a winning position for the opponent then delete the pieces that makes them winning
+            available_pieces = deepcopy(self.available_pieces)
+            for position in winning_position_opponent:
+                for piece in available_pieces:
+                    winner = self.__simulate_winning_move(position, piece)
+                    if winner:
+                        available_pieces.remove(piece)
+            if len(available_pieces) > 0:
+                return random.choice(available_pieces)
+
+        piece = random.choice(self.available_pieces)
+        return piece
+
+    def __check_line_position(self, positions, partial_board):
+        if positions.shape[0] <= 3 and positions.shape[0]:
+            quarto = deepcopy(self.get_game())
+            piece = quarto.get_selected_piece()
+            binary_representation = deepcopy(partial_board)
+            binary_representation[positions[0]] = piece
+            binary_representation = binary_representation[binary_representation != -1]
+            common_pieces_1 = reduce(lambda a, b: a & b, binary_representation)
+            common_pieces_2 = reduce(lambda a, b: a & (not b), binary_representation, 15)
+            return common_pieces_1 or common_pieces_2
+        return False
+    def __check_line_positions(self):
+
+        board = self.__get_board()
+        line_position_3_vacancy = []
+        line_position_2_vacancy = []
+
+        for i in range(4):
+            column_pos = np.where(board[i, :] == -1)[0]
+            if self.__check_line_position(column_pos, board[i, :]):
+                if len(column_pos) == 3:
+                    line_position_3_vacancy.append((column_pos[0], i))
+                else:
+                    line_position_2_vacancy.append((column_pos[0], i))
+
+            row_pos = np.where(board[:, i] == -1)[0]
+            if self.__check_line_position(row_pos, board[:, i]):
+                if len(row_pos) == 3:
+                    line_position_3_vacancy.append((i, row_pos[0]))
+                else:
+                    line_position_2_vacancy.append((i, row_pos[0]))
+
+        diag_1 = np.diag(board)
+        diag1_pos = np.where(diag_1 == -1)[0]
+        if self.__check_line_position(diag1_pos, diag_1):
+            if len(diag1_pos) == 3:
+                line_position_3_vacancy.append((diag1_pos[0], diag1_pos[0]))
+            else:
+                line_position_2_vacancy.append((diag1_pos[0], diag1_pos[0]))
+
+        diag_2 = np.diag(np.fliplr(board))
+        diag2_pos = np.where(diag_2 == -1)[0]
+        if self.__check_line_position(diag2_pos, diag_2):
+            if len(diag2_pos) == 3:
+                line_position_3_vacancy.append((3 - diag2_pos[0], diag2_pos[0]))
+            else:
+                line_position_2_vacancy.append((3 - diag2_pos[0], diag2_pos[0]))
+
+        if len(line_position_2_vacancy) > 0:
+            return line_position_2_vacancy
+        return line_position_3_vacancy
 
     def place_piece(self) -> tuple[int, int]:
-        return random.randint(0, 3), random.randint(0, 3)
+        self.__update_positions()
+        candidate_winning_position = self.__list_winning_positions()
+        if len(candidate_winning_position) > 0:
+            for position in candidate_winning_position:
+                winner = self.__simulate_winning_move(position)
+                if winner:
+                    return position[0], position[1]
+
+        candidate_line_position = self.__check_line_positions()
+
+        if len(candidate_line_position) > 0:
+                    return candidate_line_position[0][0], candidate_line_position[0][1]
+
+        x, y = random.choice(self.available_position)
+        return x, y
 

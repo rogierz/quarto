1d93a85c13df9c75eaf9529c4b6113e29c5a584c
Ruggero Nocera <9341160+rogierz@users.noreply.github.com>
2023-05-02 19:37:17 +0200

    added all_tournament image

--- heatmap.py
+++ heatmap.py
@@ -1,31 +1,29 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from players import PLAYERS
+
+# Thanks ChatGPT
 
 # Define the win rate data as a 2D array
-data = np.array([[0, 0.4975, 0.4975, 0.4975, 0.4955, 0.4975, 0.7588],
-                 [0.5125, 0, 0.8318, 0.9019, 0.4755, 0.4975, 0.7868],
-                 [0.3639, 0.1391, 0, 0.5946, 0.4884, 0.4884, 0.5065],
-                 [0.3639, 0.1151, 0.4014, 0, 0.3, 0.3, 0.5395],
-                 [0.4662, 0.5045, 0.4884, 0.7, 0, 0.4955, 0.4888],
-                 [0.5035, 0.5035, 0.4884, 0.7, 0.5055, 0, 0.4888],
-                 [0.7588, 0.2412, 0.5065, 0.4605, 0.5116, 0.5116, 0]])
-
+data = np.load("tournaments/all_tournament.npy")
 # Normalize the data between 0 and 1
-data_norm = (data - np.min(data)) / (np.max(data) - np.min(data))
+data_norm = data / 100
 
 # Define the x and y labels for the heatmap
-labels = ['rl', 'random', 'naive', 'risky',
-          'montecarlo', 'minmax', 'evolutive']
+labels = [k for k in PLAYERS.keys() if k != 'random']
 
 # Create a heatmap using Matplotlib
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(figsize=(10, 9))
 im = ax.imshow(data_norm, cmap='viridis')
 
 # Show the colorbar and set the tick labels
-cbar = ax.figure.colorbar(im, ax=ax)
-cbar.ax.set_yticklabels(['0', '0.2', '0.4', '0.6', '0.8', '1'])
+cbar = ax.figure.colorbar(im, ax=ax, use_gridspec=True)
+cbar.ax.set_yticklabels(
+    ['0', '0.2', '0.4', '0.6', '0.8', '1'])
 
 # Set the x and y axis tick labels
+ax.set_ylabel("Player 0", fontsize=14)
+ax.set_xlabel("Player 1", fontsize=14)
 ax.set_xticks(np.arange(len(labels)))
 ax.set_yticks(np.arange(len(labels)))
 ax.set_xticklabels(labels)
@@ -45,4 +43,5 @@ for i in range(len(labels)):
 ax.set_title("Win Rate Heatmap")
 
 # Show the heatmap
+plt.savefig("images/all_tournaments.png", dpi=600)
 plt.show()
